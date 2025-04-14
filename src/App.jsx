a2b1b46fb7ca91967280348b5dbb0984faa9ae07
nickmenegussi <nicolasmramos09@gmail.com>
2025-04-14 20:32:54 -0300

    refactor: removendo páginas que serviram  apenas para desenvolver o design e finalizar algum protótipo.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
-import Cadastro from './pages/cadastro';
-import Jogo from './pages/jogo';
-import Status from './pages/status';
-import Resultado from './pages/resultado';
-import BugFixMenu from './pages/MenuBug';
+// import Cadastro from './pages/cadastro';
+// import Jogo from './pages/jogo';
+// import Status from './pages/status';
+// import Resultado from './pages/resultado';
+// import BugFixMenu from './pages/MenuBug';
 
 
 export default function App() {
@@ -13,11 +13,11 @@ export default function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/cadastro" element={<Cadastro />} />
+        {/* <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/jogo" element={<Jogo />} />
         <Route path="/status" element={<Status />} />
         <Route path="/resultado" element={<Resultado />} />
-        <Route path="/menuBug" element={<BugFixMenu />} />
+        <Route path="/menuBug" element={<BugFixMenu />} /> */}
       </Routes>
     </Router>
   );
